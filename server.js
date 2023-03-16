487152b564ae72be986e1a29c2254826b6d88818
Sania Malik <111688561+saniamalik119@users.noreply.github.com>
2023-03-16 13:06:56 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+app.listen(process.env.PORT || 3000, ()=> console.log("everything is ok"))
